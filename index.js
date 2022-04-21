31dae81d7e5b24c6f8c4c77e77fdae7a54bf678b
Zachary Boyd <zachary@zacharyboyd.nyc>
2022-04-20 23:09:25 -0400

    skips non-images

--- index.js
+++ index.js
@@ -5,14 +5,25 @@ const cheerio = require('cheerio');
 const port = Number(process.env.PORT) || 3000;
 
 app.get('/', (req, res) => {
-  fetch('https://commons.wikimedia.org/wiki/Special:Random/File').then(async (resp) => {
-    const text = await resp.text();
-    const $ = cheerio.load(text);
+  (async () => {
+    for (const i = 0; i < 50; i++) {
+      try {
+        const resp = await fetch('https://commons.wikimedia.org/wiki/Special:Random/File');
+        const text = await resp.text();
+        const $ = cheerio.load(text);
 
-    const url = $('.fullMedia a.internal').attr('href');
+        const url = $('.fullMedia a.internal').attr('href');
 
-    res.redirect(302, url);
-  });
+        if (url.match(/\.(svg|jpg|png|)$/)) {
+          res.redirect(302, url);
+          break;
+        }
+      } catch (err) {
+        console.error(err.stack);
+        continue;
+      }
+    }
+  })();
 });
 
 app.listen(port, () => {
